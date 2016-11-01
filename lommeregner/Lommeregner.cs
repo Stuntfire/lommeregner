f39e9432c1f783d5d6f39b4b54e4b215c57a0c5a
Rudi Persson <rudi0234@edu.easj.dk>
2016-11-01 10:38:33 +0100

    Add project files.

---
+++ lommeregner/Lommeregner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lommeregner
+{
+    class Lommeregner
+    {
+        public string input1 { get; set; }
+        //public string input1 = Console.ReadLine();
+        //int output2;
+        //int number1 = Int32.Parse(input1);
+
+        public int test(string input)
+        {
+            int number1 = Int32.Parse(input);
+            return number1;
+        }
+
+
+    }
+}
---
+++ lommeregner/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lommeregner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string input1 = Console.ReadLine();
+            int output2;
+            int number1 = Int32.Parse(input1);
+
+        }
+    }
+}
